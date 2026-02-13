a9eccdd6a49998ccc29f2ee6966a141ee03600e9
Aayush <aayush.buchade@gmail.com>
2026-02-13 22:26:52 +0530

    100 days of python

---
+++ Day 22/pong_game.py
@@ -0,0 +1,13 @@
+from turtle import Screen
+
+
+def main():
+
+    screen = Screen()
+    screen.setup(width=1000, height=600)
+    screen.bgcolor('black')
+    screen.tracer(0)
+    screen.colormode(255)
+    screen.exitonclick()
+
+main()
